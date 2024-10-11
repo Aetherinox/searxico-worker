616729351207c349cb1b8ac5e8416f236a37da69
Aetherinox <aetherinox@proton.me>
2024-10-11 11:29:03 -0700

    change(json): support for status errors

--- src/json.js
+++ src/json.js
@@ -8,30 +8,40 @@ import types from './types.js'
     Json > Format
 */
 
-const formatJSON = (obj, pretty) => JSON.stringify(obj, null, pretty ? 2 : 0)
+const formatJSON = (arr, pretty) => JSON.stringify(arr, null, pretty ? 2 : 0)
 
 /*
     Json > Response
+
+    @arg        array arr
+    @arg        int code
+    #arg        bool pretty
 */
 
-const jsonResp = (obj, pretty) => {
-    return new Response(formatJSON(obj, pretty), {
+const jsonResp = (arr, code, pretty) => {
+    return new Response(formatJSON(arr, pretty), {
         headers: {
             'content-type': types.json,
             'Access-Control-Allow-Origin': '*'
-        }
+        },
+        status: code
     })
 }
 
 /*
     Json > Error
+
+    @usage      return jsonErr( { message: "not found" }, 404, true)
+                return jsonErr("not found", 404, true)
+
+    @arg        str error
+    @arg        int code
+    #arg        bool pretty
 */
 
 
-const jsonErr = (error, pretty) => {
-    return jsonResp({
-        error: typeof error === 'string' ? error : error.message
-    }, pretty)
+const jsonErr = (error, code, pretty) => {
+    return jsonResp({ response: typeof error === 'string' ? error : error.message, code: code }, code, pretty)
 }
 
 /*
